1a301db21f85893b4a86d354b3fa3a76fc078854
grupo 2 <jasf5@aluno.ifal.edu.br>
2024-08-19 14:26:27 -0300

    inicio da implantação de conta

---
+++ banco.java
@@ -0,0 +1,5 @@
+public class banco {
+    public void criar_conta(String titular, double saldo_inicial){
+        
+    }
+}
---
+++ conta.java
@@ -0,0 +1,13 @@
+public class conta{
+
+ int numero;
+ String titular;
+ double saldo;   
+
+ public static void depositar(double valor){
+
+ }
+ public static void verificar_saldo(){
+    
+ }
+}
